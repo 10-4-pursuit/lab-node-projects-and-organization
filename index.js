9aeb20f369ebfc229d3a8a3dfdfa84e6689c8eb4
USERNAME <EMAIL>
2023-10-07 02:34:03 -0400

    completed all except the bonus question

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
 });
 
 function allWords(){
+	console.log("All of The Words");
 	console.log(words);
 }
 
@@ -18,6 +19,7 @@ function firstTenWords(){
 	 wordHolder.push(words[i])
 	}
 	const firstTenWordsList = wordHolder;
+	console.log("First Ten Words");
 	console.log(firstTenWordsList);
 	} 
 
@@ -29,6 +31,7 @@ function nextTenWords(){
 		wordHolder.push(words[i])
 	}
 	const nextTenList = wordHolder;
+	console.log('Next Ten Words');
 	console.log(nextTenList);
 }
 nextTenWords();
@@ -42,6 +45,7 @@ const wordsWithXList = [];
 		wordsWithXList.push(word);
 	}	
 	}
+	console.log("All Words With X in it");
 console.log(wordsWithXList);
 }
 
@@ -57,12 +61,14 @@ function subsetOfWords(){
 			wordsWithXAndYList.push(word)
 		}
 	}
+	console.log("All Words With X and Y in it ")
 	console.log(wordsWithXAndYList);
 }
 subsetOfWords();
 
 function sortWords(){
 	const sortedWords = [...words].sort();
+	console.log("All Words in Alphabet Order");
 	console.log(sortedWords);
 }
 
@@ -77,27 +83,142 @@ function wordsWithQ(){
 		wordsWithQList.push(word)
 	}
 	}
+	console.log("All Words With Q in it");
 	console.log(wordsWithQList);
 }
 wordsWithQ();
 
 
- function findWordsWithLetters(words, letter){
- const matchingWords = [];
-
- for (let i = 0; i < words.length; i++) {
- 	const word = words[i];
- if (word.includes(letter.toLowerCase())){
- 	matchingWords.push(word);
- }	
- }
- console.log(matchingWords);
- }
-rl.question('Enter a letter: ', (letter) => {
+function findWordsWithLetters(words, letter) {
+	const matchingWords = [];
+	const wordsWithTwoOrMore = [];
+	const wordsWithoutLetter = [];
+  
+	for (let i = 0; i < words.length; i++) {
+	  const word = words[i];
+	  if (word.includes(letter.toLowerCase())) {
+		matchingWords.push(word);
+	  }
+	  const letterCount = word.split('').filter((char) => char === letter.toLowerCase()).length;
+	  if (letterCount >= 2) {
+		wordsWithTwoOrMore.push(word);
+	  }
+	  if (!word.includes(letter.toLowerCase())) {
+		wordsWithoutLetter.push(word);
+	  }
+	}
+  
+	if (matchingWords.length > 0) {
+	  console.log(`Words with the letter '${letter}':`);
+	   console.log(matchingWords);
+	  console.log(`Words with two or more '${letter}' letters:`);
+	  console.log(wordsWithTwoOrMore);
+	} else {
+	  console.log(`No words found with matching letter '${letter}'.`);
+	}
+	if (wordsWithoutLetter.length > 0) {
+		console.log(`Words without the letter '${letter}':`);
+		console.log(wordsWithoutLetter);
+	  }
+  }
+  
+  rl.question('Enter a letter: ', (letter) => {
 	const inputLetter = letter.toLowerCase();
- findWordsWithLetters(words, inputLetter);
-rl.close();
-}); 
+	findWordsWithLetters(words, inputLetter);
+	rl.close();
+  });
+  
+// function lettersMatch(letters){
+// 	const matchingWords = [];
 
+// 	for(const word of words) {
+// 	let wordMatches = true;
 
-// function letterMatch()
+// 		for(const letter of letters){
+// 		  if (!word.includes(letter)) {
+// 			wordMatches = false;
+// 			break;
+// 			  }
+// 			} 
+// 		if (wordMatches) {
+// 			matchingWords.push(word);
+// 		}
+// 	}
+// 	return matchingWords;
+// }
+// function lettersExactMatch(word, pattern) {
+// 	if (word.length !== 5 || pattern.length !== 5) {
+// 	  console.log('Please enter a five-character word and pattern.');
+// 	  return;
+// 	}
+
+// 	let result = '';
+// 	for (let i = 0; i < 5; i++) {
+// 	  if (pattern[i] === '_') {
+// 		result += '.';
+// 	  } else if (pattern[i] === word[i]) {
+// 		result += word[i];
+// 	  } else {
+// 		result += '_';
+// 	  }
+// 	}
+
+// 	console.log(result);
+// }
+// function lettersCombinedMatch(word, wrongPositionPattern, correctPositionPattern) {
+// 	if (word.length !== 5 || wrongPositionPattern.length !== 5 || correctPositionPattern.length !== 5) {
+// 	  console.log('Please enter five-character inputs.');
+// 	  return;
+// 	}
+  
+// 	let result = '';
+// 	for (let i = 0; i < 5; i++) {
+// 	  if (correctPositionPattern[i] === word[i]) {
+// 		result += word[i];
+// 	  } else if (wrongPositionPattern[i] === '_') {
+// 		if (word.includes(correctPositionPattern[i])) {
+// 		  result += '.';
+// 		} else {
+// 		  result += '_';
+// 		}
+// 	  } else if (wrongPositionPattern[i] === word[i]) {
+// 		result += word[i];
+// 	  } else {
+// 		result += '_';
+// 	  }
+// 	}
+  
+// 	console.log(result);
+//   }
+//   rl.question('Enter a word or letters for matching: ', (input) => {
+// 	const inputLetters = input.toLowerCase().split('');
+  
+// 	if (inputLetters.length > 5) {
+// 	  console.log('Please enter up to 5 letters.');
+// 	  rl.close();
+// 	  return;
+// 	}
+  
+// 	const matchingWords = lettersMatch(inputLetters);
+  
+// 	if (matchingWords.length === 0) {
+// 	  console.log('No words found containing these letters');
+// 	} else {
+// 	  console.log(`Words containing these letters: ${matchingWords.join(', ')}`);
+// 	}
+  
+// 	rl.question('Enter a word for exact matching: ', (word) => {
+// 	  rl.question('Enter an exact matching pattern: ', (pattern) => {
+// 		lettersExactMatch(word, pattern);
+  
+// 		rl.question('Enter a word for combined matching: ', (wordCombined) => {
+// 		  rl.question('Enter a wrong position pattern: ', (wrongPositionPattern) => {
+// 			rl.question('Enter a correct position pattern: ', (correctPositionPattern) => {
+// 			  lettersCombinedMatch(wordCombined, wrongPositionPattern, correctPositionPattern);
+// 			  rl.close();
+// 			});
+// 		  });
+// 		});
+// 	  });
+// 	});
+//   });
