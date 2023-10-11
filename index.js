030e1bc5390825ff0a85e0a2f3f59782ec23d491
EdgarCollado <edgarcollado@pursuit.org>
2023-10-11 18:22:10 -0400

    updated-functions-and-formatted-code

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const words = require("./words.json");
 
 function allWords() {
-    console.log(words);
+  console.log(words);
 }
 
 allWords();
 
 function firstTenWords() {
-    const wordHolder = [];
-    for(let i = 0; i < 10; i++) {
-        wordHolder.push(words[i])
-    }
-    console.log(wordHolder);
+  const wordHolder = [];
+  for (let i = 0; i < 10; i++) {
+    wordHolder.push(words[i]);
+  }
+  console.log(wordHolder);
 }
 
 firstTenWords();
 
 function nextTenWords() {
-    const nextTenWordHolder = [];
-    for (let i = 10; i <= 19; i++) {
-        nextTenWordHolder.push(words[i])
-    }
-    console.log(nextTenWordHolder);
+  const nextTenWordHolder = [];
+  for (let i = 10; i <= 19; i++) {
+    nextTenWordHolder.push(words[i]);
+  }
+  console.log(nextTenWordHolder);
 }
 
 nextTenWords();
 
 function firstXWords(x) {
-    console.log(words.slice(0, x));
+  console.log(words.slice(0, x));
 }
 
 firstXWords(13);
@@ -39,19 +39,19 @@ function subsetOfWords(x, y) {
 subsetOfWords(0, 100);
 
 function sortWords() {
-    console.log(words.sort());
+  console.log(words.sort());
 }
 
 sortWords();
 
 function wordsWithQ() {
-    let specificWords = [];
-    for (const word of words) {
-        if (word.includes("q")) {
-            specificWords.push(word)
-        }
+  let specificWords = [];
+  for (const word of words) {
+    if (word.includes("q")) {
+      specificWords.push(word);
     }
-    console.log(specificWords);
+  }
+  console.log(specificWords);
 }
 
 wordsWithQ();
@@ -63,37 +63,34 @@ const letter = process.argv[2];
 const halvedWords = words.slice(0, 5);
 
 function findWordsWithLetter() {
-    let specificLetter = [];
+  let specificLetter = [];
 
-    for(const word of halvedWords) {
-        if (word.includes(letter)) {
-            specificLetter.push(word)
-        }
+  for (const word of halvedWords) {
+    if (word.includes(letter)) {
+      specificLetter.push(word);
     }
-    console.log(specificLetter);
+  }
+  console.log(specificLetter);
 }
 
 findWordsWithLetter();
 
 function lettersMatch(guess) {
-    if (guess.length !== 5) {
-        return `please enter 5 letter word`
+  if (guess.length !== 5) {
+    return `please enter 5 letter word`;
+  }
+  const filteredWords = words.filter((word) => {
+    for (let letter of guess) {
+      if (word.includes(letter)) {
+        return true;
+      }
     }
-    const filteredWords = words.filter(word => {
-        for (let letter of guess) {
-            if (word.includes(letter)) {
-                return true
-            }
-        }
-        return false
-    })
-    return filteredWords
+    return false;
+  });
+  return filteredWords;
 }
 
 console.log(lettersMatch(`lodge`));
-
-
-
 
 function letttersExactMatch(guess, target) {
   if (guess.length !== 5) {
@@ -111,4 +108,3 @@ function letttersExactMatch(guess, target) {
 }
 
 console.log(letttersExactMatch(`acorn`, `adorn`));
-
